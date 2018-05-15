15195496964f8bf4d0c0d0d1fa756f3524d00675
TPU\ram8 <ram290495@gmail.com>
2018-05-15 17:44:19 +0700

    +Некоторые поправки

--- niftiStream.py
+++ niftiStream.py
@@ -4,12 +4,15 @@ import nibabel as nib
 
 class NiftiStream:
     def __init__(self):
-        self.__images = np.ndarray([0], dtype=np.uint8)
+        self.__images = None
 
     def load_file(self, filename):
-        current_file_data = nib.load(filename)
+        current_file_data = nib.load(filename).get_data()
         # print(current_file_data.shape)
-        self.__images = current_file_data
+        if self.__images is None:
+            self.__images = current_file_data
+        else:
+            self.__images.append([current_file_data])
 
     def get_images(self):
         return self.__images
